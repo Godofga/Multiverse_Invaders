5c84d13dbfc68fa8157412e34ce629473cad02c9
Godofga <igor_mcampos@hotmail.com>
2019-01-15 08:13:05 -0300

    maybe one of thhe final ones

--- Javascript/P5JSGAME/sons.js
+++ Javascript/P5JSGAME/sons.js
@@ -1,8 +1,9 @@
 var estouro= [];
 var vitori;
+var musica;
 function spop()
 {
-  estouro[round(random(0,estouro.length-1))].volume(1);
+  estouro[round(random(0,estouro.length-1))].volume(0.6);
   estouro[round(random(0,estouro.length-1))].play();
 
 };
@@ -11,17 +12,26 @@ function victowry()
   vitori.volume(1);
   vitori.play();
 }
+function musicyea()
+{
+  /*Atmospheric French-style chillout with a retro beat, accompanied by a Guitars, Rhodes, bass, synth pads and arpeggios, ambience, percussion and dub effects. Conveys a romantically dreamy mood.
+Ideal for Lifestyle, fashion, sports, innovative technology, bar and lounge movies. 117bpm (Artist: Vodovoz) */
+  musica.volume(0.5);
+  musica.loop();
+}
 function loade()
 {
-  estouro.push(createAudio('./audio/pop1.mp3'));
-  estouro.push(createAudio('./audio/pop2.mp3'));
-  estouro.push(createAudio('./audio/pop3.mp3'));
-  estouro.push(createAudio('./audio/pop4.mp3'));
-  estouro.push(createAudio('./audio/pop5.mp3'));
-  estouro.push(createAudio('./audio/pop1.mp3'));
-  estouro.push(createAudio('./audio/pop2.mp3'));
-  estouro.push(createAudio('./audio/pop3.mp3'));
-  estouro.push(createAudio('./audio/pop4.mp3'));
-  estouro.push(createAudio('./audio/pop5.mp3'));
-  vitori= createAudio('./audio/win.mp3');
+  estouro.push(createAudio('./audio/P5JSGAME/pop1.mp3'));
+  estouro.push(createAudio('./audio/P5JSGAME/pop2.mp3'));
+  estouro.push(createAudio('./audio/P5JSGAME/pop3.mp3'));
+  estouro.push(createAudio('./audio/P5JSGAME/pop4.mp3'));
+  estouro.push(createAudio('./audio/P5JSGAME/pop5.mp3'));
+  estouro.push(createAudio('./audio/P5JSGAME/pop1.mp3'));
+  estouro.push(createAudio('./audio/P5JSGAME/pop2.mp3'));
+  estouro.push(createAudio('./audio/P5JSGAME/pop3.mp3'));
+  estouro.push(createAudio('./audio/P5JSGAME/pop4.mp3'));
+  estouro.push(createAudio('./audio/P5JSGAME/pop5.mp3'));
+  vitori= createAudio('./audio/P5JSGAME/win.mp3');
+  musica= createAudio('./audio/P5JSGAME/Come Closer.mp3');
+  musicyea();
 }
